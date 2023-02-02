0b79ea09571434bde9f8496102a86199fd5b46e1
TianDaGe <tiandage719@outlook.com>
2023-02-02 12:03:26 +0800

    updated style

--- src/components/Views/Home/TopRafflers.js
+++ src/components/Views/Home/TopRafflers.js
@@ -15,9 +15,9 @@ const RafflerCard = ({ data }) => {
                     </div>
                     <div className="cs-card_media_right w-100">
                         <a href={`/profile/${data.wallet}`}>
-                            <h3>
+                            <h4>
                                 {data?.username ? `@${data?.username.trimStart()}` : getSummaryAddress(data?.wallet)}
-                            </h3>
+                            </h4>
                         </a>
                         <div className="cs-card_media_info w-100">
                             <div><i className="fas fa-ticket-alt fa-fw"></i> {getNumberFormat1(data.count)} Raffles</div>
