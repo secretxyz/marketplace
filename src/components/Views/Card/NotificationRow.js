e3469220a6ba7378360b43ca576288f4dc4543a4
TianDaGe <tiandage719@outlook.com>
2023-02-05 15:12:51 +0800

    checked undefined in notification row

--- src/components/Views/Card/NotificationRow.js
+++ src/components/Views/Card/NotificationRow.js
@@ -1,13 +1,13 @@
 import { getSummaryUsername } from "../../Helpers/Utils";
 
 const NotificationRow = ({ data }) => {
-    const nft = data.nft.data.attributes;
-    const from = data.from.data.attributes;
+    const nft = data.nft?.data?.attributes;
+    const from = data.from?.data?.attributes;
     const raffle = {
-        ...data.raffle.data.attributes,
-        id: data.raffle.data.id,
+        ...data.raffle?.data?.attributes,
+        id: data.raffle?.data?.id,
     };
-    const raffle_ticket = data.raffle_ticket.data.attributes;
+    const raffle_ticket = data.raffle_ticket?.data?.attributes;
 
     const generateMessage = () => {
         switch (data.activity) {
@@ -34,11 +34,11 @@ const NotificationRow = ({ data }) => {
     }
 
     return (
-        <a href={`/nft/${nft.nft_tokenid}/${raffle.id}`} className="cs-notification_item">
-            <div className="cs-notification_thumb"><img src={nft.picture_url} alt="Image" /></div>
+        <a href={`/nft/${nft?.nft_tokenid}/${raffle?.id}`} className="cs-notification_item">
+            <div className="cs-notification_thumb"><img src={nft?.picture_url} alt="Image" /></div>
             <div className="cs-notification_right">
                 <p>{generateMessage()}</p>
-                <h4>{nft.name}</h4>
+                <h4>{nft?.name}</h4>
             </div>
         </a>
     )
