7da7829fc8f8f6a705cc8486c2fa59f3bd752266
TianDaGe <tiandage719@outlook.com>
2023-02-27 17:17:03 +0800

    added helmet provider

--- src/components/Layout/Base.js
+++ src/components/Layout/Base.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Helmet } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import Header from './Header'
 import Footer from './Footer'
@@ -15,49 +15,45 @@ const Base = (props) => {
     }, [])
 
     return (
-        <div className="main-content">
-            {/* <Helmet>
-                <title>SecretRaffles</title>
-                <meta name="description" content="Quickly enter or create raffles for unique digital assets. Join the exciting world of NFT raffles on SecretRaffle now!" />
-                <meta property="og:url" content="https://secretraffles.xyz" />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="SecretRaffles" />
-                <meta property="og:description" content="Quickly enter or create raffles for unique digital assets. Join the exciting world of NFT raffles on SecretRaffle now!" />
-                <meta property="og:image" content="https://secretraffles.xyz/img/secret_logo.png" />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta property="twitter:domain" content="secretraffles.xyz" />
-                <meta property="twitter:url" content="https://secretraffles.xyz" />
-                <meta name="twitter:title" content="SecretRaffles" />
-                <meta name="twitter:description" content="Quickly enter or create raffles for unique digital assets. Join the exciting world of NFT raffles on SecretRaffle now!" />
-                <meta name="twitter:image" content="https://secretraffles.xyz/img/secret_logo.png" />
-            </Helmet> */}
+        <HelmetProvider>
+            <div className="main-content">
+                <Helmet>
+                    <title>SecretRaffles</title>
+                    <meta name="description" content="Quickly enter or create raffles for unique digital assets. Join the exciting world of NFT raffles on SecretRaffle now!" />
+                    <meta property="og:url" content="https://secretraffles.xyz" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:title" content="SecretRaffles" />
+                    <meta property="og:description" content="Quickly enter or create raffles for unique digital assets. Join the exciting world of NFT raffles on SecretRaffle now!" />
+                    <meta property="og:image" content="https://secretraffles.xyz/img/secret_logo.png" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:domain" content="secretraffles.xyz" />
+                    <meta property="twitter:url" content="https://secretraffles.xyz" />
+                    <meta name="twitter:title" content="SecretRaffles" />
+                    <meta name="twitter:description" content="Quickly enter or create raffles for unique digital assets. Join the exciting world of NFT raffles on SecretRaffle now!" />
+                    <meta name="twitter:image" content="https://secretraffles.xyz/img/secret_logo.png" />
+                </Helmet>
 
-            <Header />
+                <Header />
 
-            <main>
-                {props.children}
-            </main>
+                <main>
+                    {props.children}
+                </main>
 
-            <Footer />
+                <Footer />
 
-
-            {/* <div className="cs-modal_overlay">
-                Loading...
-            </div> */}
-
-
-            <ToastContainer position="top-right"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss={false}
-                draggable={false}
-                pauseOnHover={false}
-                theme={getThemeMode() ? "light" : "dark"}
-            />
-        </div>
+                <ToastContainer position="top-right"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss={false}
+                    draggable={false}
+                    pauseOnHover={false}
+                    theme={getThemeMode() ? "light" : "dark"}
+                />
+            </div>
+        </HelmetProvider>
     )
 }
 
