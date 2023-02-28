049418d7e5ebf36933a73889ec42a732a7f05e38
TianDaGe <tiandage719@outlook.com>
2023-02-28 10:33:46 +0800

    fixed header bar

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -107,7 +107,7 @@ const Header = () => {
 														<li><a href="/faq">FAQ</a></li>
 													</ul>
 												</li>
-												<li className="cs-menu_footer_widget">
+												{/* <li className="cs-menu_footer_widget">
 													<form className="cs-footer_newsletter">
 														<input type="text" placeholder="Subscribe to our newsletter" className="cs-newsletter_input" />
 														<button className="cs-newsletter_btn">
@@ -121,7 +121,7 @@ const Header = () => {
 														<a href="https://twitter.com/SecretLabsXRPL" target="_blank"><i className="fab fa-twitter fa-fw"></i></a>
 														<a href="https://discord.gg/VnNGWAAj3Q" target="_blank"><i className="fab fa-discord fa-fw"></i></a>
 													</div>
-												</li>
+												</li> */}
 											</ul>
 										</div>
 									</div>
@@ -130,12 +130,12 @@ const Header = () => {
 							<div className="cs-header_btns_wrap">
 								<div className="cs-header_btns">
 									<SwitchMode />
-									<div className="cs-header_icon_btn cs-center cs-mobile_search_toggle">
+									{/* <div className="cs-header_icon_btn cs-center cs-mobile_search_toggle">
 										<svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
 											<path d="M9.16667 16.3333C12.8486 16.3333 15.8333 13.3486 15.8333 9.66667C15.8333 5.98477 12.8486 3 9.16667 3C5.48477 3 2.5 5.98477 2.5 9.66667C2.5 13.3486 5.48477 16.3333 9.16667 16.3333Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
 											<path d="M17.5 18L13.875 14.375" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
 										</svg>
-									</div>
+									</div> */}
 									{auth_token && <NotificationBox accountId={account?.id} />}
 									{auth_token && <ProfileBox account={account} />}
 									{!auth_token && <button className="cs-btn cs-style1" onClick={() => { onConnectWallet() }}>
